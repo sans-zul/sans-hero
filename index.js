ad1e3206a78acdd55e13959ebdb394c07933a702
Sanjari Alfian <sanjari.alfian.01@gmail.com>
2019-12-26 18:21:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
     res.send("Hello World!");
 });
 
-app.listen(3000);
+app.listen(port, function () {
+    console.log(`Server Starts on ${port}`);
+});
